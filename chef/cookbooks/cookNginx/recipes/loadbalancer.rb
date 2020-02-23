d63f3ebfd2ddf37992b76e468ee9825957d19184
Henry <henryakinola59@gmail.com>
2020-02-23 23:11:08 +0000

    typo correction in template directory

--- chef/cookbooks/cookNginx/recipes/loadbalancer.rb
+++ chef/cookbooks/cookNginx/recipes/loadbalancer.rb
@@ -3,6 +3,6 @@
 
 #overwritting the default nginx server configuratio
 template '/etc/nginx/sites-available/default' do
-    source 'etc\nginx.erb'
+    source 'etc\nginx-default.erb'
     action :create
 end
